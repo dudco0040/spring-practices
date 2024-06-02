96660f6ed76ac6c9c9f6dfad77cbef7ae88bde16
BITCOM <BITCOM@BIT>
2024-06-03 02:43:23 +0900

    5/31 spring 수업 실습

--- hellospring/src/main/java/com/poscodx/hellospring/controller/GuestbookController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/GuestbookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @RequestMapping 클래스 단독 매핑
  * Spring MVC 4.x 지원
  */
-@Controller  // 이걸 주석처리 하면 컨트롤러가 생기지 않으면서 에러가 발생하지 않을 것 
+// @Controller  // 이걸 주석처리 하면 컨트롤러가 생기지 않으면서 에러가 발생하지 않을 것 
 @RequestMapping("/guestbook/*")
 public class GuestbookController {
 	
--- hellospring/src/main/java/com/poscodx/hellospring/controller/UserController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/UserController.java
@@ -2,19 +2,48 @@ package com.poscodx.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @RequstMapping 클래스 + 메소드 매핑 - 추천하는 방법
  */
 
 @Controller
-@RequestMapping("/uesr")  // 공통된 URL - 클래스에서 지정
+@RequestMapping("/user")  // 공통된 URL - 클래스에서 지정
 public class UserController {
 	
-	@ResponseBody
-	@RequestMapping("/join")  // 기능에 따른 URL - 메소드에서 지정
+	@RequestMapping(value ="/join", method=RequestMethod.GET)  // 기능에 따른 URL - 메소드에서 지정
 	public String join() {
-		return "UserController.join()";
-		
-	}	
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value ="/join", method=RequestMethod.POST)  // 기능에 따른 URL - 메소드에서 지정
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/**
+		 *  만일 n이라는 이름의 파라미터가 없으면 
+		 *  404 Bad Request Error 가 발생한다. 
+		 */
+		return "UserController.update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n", required=true, defaultValue="") String name) {
+		return "UserController.update(" + name + ")";
+	}
+
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value="p", required=true, defaultValue="1") int pageNo) {
+		return "UserController.list(" + pageNo + ")";
+	}
+	
 }
