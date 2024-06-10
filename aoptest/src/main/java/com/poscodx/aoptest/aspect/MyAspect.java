85b3ca5530eb009e5f42ed99546b9d63a02d5c82
BITCOM <BITCOM@BIT>
2024-06-10 14:52:44 +0900

    6/10 AOP#2 Join Point2

--- aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
@@ -1,7 +1,10 @@
 package com.poscodx.aoptest.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -26,5 +29,24 @@ public class MyAspect {
 		System.out.println("--- AfterReturning Advice ---");
 	}
 	
+	@AfterThrowing(value = "execution(* *..*.ProductService.*(..))", throwing="ex")  // turn, parameter 생략 
+	public void adviceAfterThrowing(Throwable ex) {
+		System.out.println("--- AfterThrowing Advice ---" + ex + "---");
+	}
 	
+	@Around(value = "execution(* *..*.ProductService.*(..))")
+	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
+		/*Before*/
+		System.out.println("--- Around(Before) Advice ---");
+		
+		/*Point Cut Method 실행*/
+		Object[] params = {"Camera"};
+		Object result = pjp.proceed(params);  // 객체를 받아서 return 
+		
+		/*After*/
+		System.out.println("--- Around(After) Advice ---");
+		
+		return result;
+	}
+
 }
